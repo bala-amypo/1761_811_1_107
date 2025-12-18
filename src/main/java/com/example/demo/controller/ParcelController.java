2691542c8a85da75821dfbb2f7ee1b25a70328ca
bala-amypo <bala@amypo.in>
2025-12-18 09:00:15 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/ParcelController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Parcel;
+import com.example.demo.service.ParcelService;
+
+@RestController
+public class ParcelController {
+    @Autowired
+    ParcelService ser;
+
+    @PostMapping("/addParcel")
+    public Parcel addParcel(@RequestBody Parcel p) {
+        return ser.addParcel(p);
+    }
+    @GetMapping("/getParcels/{trackingNumber}")
+    public Parcel getParcel(@PathVariable String trackingNumber) {
+        return ser.getParcel(trackingNumber);
+    }
+
+    
+}
---
+++ src/main/java/com/example/demo/repository/ParcelRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.model.Parcel;
+
+public interface ParcelRepository extends JpaRepository<Parcel, Long> {
+
+
+    Parcel findByTrackingNumber(String trackingNumber);
+
+    
+} 
