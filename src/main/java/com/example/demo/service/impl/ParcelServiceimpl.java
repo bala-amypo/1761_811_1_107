7f1b0e86cda5046e92903c66b6fba9582abb6dd9
bala-amypo <bala@amypo.in>
2025-12-18 09:13:14 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ParcelServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/ParcelServiceimpl.java
@@ -1,11 +1,13 @@
 package com.example.demo.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Parcel;
 import com.example.demo.repository.ParcelRepository;
 import com.example.demo.service.ParcelService;
 
+@Service
 public class ParcelServiceimpl implements ParcelService {
 
     @Autowired
