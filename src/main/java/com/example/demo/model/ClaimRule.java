a3a2f5efb1f2b7f2b3771b790702ce17d3f8ea45
bala-amypo <bala@amypo.in>
2025-12-19 16:20:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/ClaimRule.java
@@ -0,0 +1,25 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "claim_rules")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClaimRule {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String ruleName;
+    private String conditionExpression;
+    private Double weight;
+
+    public ClaimRule(String ruleName, String conditionExpression, Double weight) {
+        this.ruleName = ruleName;
+        this.conditionExpression = conditionExpression;
+        this.weight = weight;
+    }
+}
