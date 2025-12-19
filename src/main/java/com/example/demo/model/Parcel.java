ae0c15ebded32f56d24f1db25a5baedc688113ab
bala-amypo <bala@amypo.in>
2025-12-19 09:10:55 +0000

    User commit

--- src/main/java/com/example/demo/model/Parcel.java
+++ src/main/java/com/example/demo/model/Parcel.java
@@ -9,7 +9,7 @@ import jakarta.persistence.Id;
 //import jakarta.persistence.GeneratedValue;
 //import jakarta.persistence.GenerationType;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Min;
 
 @Entity
 public class Parcel {
