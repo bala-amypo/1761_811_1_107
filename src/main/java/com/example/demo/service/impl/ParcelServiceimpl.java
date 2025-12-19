ea4dbc942fb6c1e19276f5a2fee6a180e6e99b9c
bala-amypo <bala@amypo.in>
2025-12-19 08:44:20 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ParcelServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/ParcelServiceimpl.java
@@ -1,8 +1,13 @@
 package com.example.demo.service.impl;
 
+
+
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Parcel;
 import com.example.demo.repository.ParcelRepository;
 import com.example.demo.service.ParcelService;
@@ -18,7 +23,8 @@ public class ParcelServiceimpl implements ParcelService {
         return repo.save(p);
     }
     @Override
-    public Parcel getParcel(String trackingNumber){
-        return repo.findByTrackingNumber(trackingNumber);
+    public Optional<Parcel> getParcel(String trackingNumber){
+        // return repo.findByTrackingNumber(trackingNumber).orElseThrow(() -> new ResourceNotFoundException("not"));
+        return Optional.ofNullable(repo.findByTrackingNumber(trackingNumber).orElseThrow(() -> new ResourceNotFoundException("not")));
     }
 }
