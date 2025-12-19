e729704b869e83e7beea3eaa04d55170ac1df344
bala-amypo <bala@amypo.in>
2025-12-19 17:43:18 +0000

    User commit

--- src/main/java/com/example/demo/model/Parcel.java
+++ src/main/java/com/example/demo/model/Parcel.java
@@ -24,7 +24,7 @@ public class Parcel {
     //private List<DamageClaim> claims;
 
     public Parcel() {
-        
+
     }
 
     
@@ -37,8 +37,8 @@ public class Parcel {
     }
 
     // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    //public Long getId() { return id; }
+    //public void setId(Long id) { this.id = id; }
     public String getTrackingNumber() { return trackingNumber; }
     public void setTrackingNumber(String trackingNumber) { this.trackingNumber = trackingNumber; }
     public String getSenderName() { return senderName; }
